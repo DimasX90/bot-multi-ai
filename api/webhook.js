150cd2f9c2857baa0505be59b520d21880d1b5b2
DimasX90 <warimanwarima@gmail.com>
2026-06-18 18:46:59 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -319,5 +319,5 @@ export default async function handler(request) {
     console.error("Global Error:", error);
     return new Response(JSON.stringify({ status: 'error' }), { status: 200 });
   }
-                                                                                      }
-                                              
+  }
+            
